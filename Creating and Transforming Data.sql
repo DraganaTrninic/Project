18fe93d663f1b444efb06ba5153d9edff23e0e69
DraganaTrninic <142996066+DraganaTrninic@users.noreply.github.com>
2023-08-24 11:26:07 +0200

    Update Creating and Transforming Data.sql

--- Creating and Transforming Data.sql
+++ Creating and Transforming Data.sql
@@ -1,4 +1,8 @@
---Concatenation (with NULL). Here I want to create a column called "Full Name" which includes the full name and title for employees in a company rather than having the name and title split up into multiple columns e.g. [FirstName], [MiddleName], etc. Because the [Title], [MiddleName], and [Suffix] fields can be NULL I have to account for that, which I do using the ISNULL function.
+/*Concatenation (with NULL). Using the AdventureWorks2019 database of a fictitional company,
+I want to create a column called "Full Name" which includes the full name and title for 
+employees in a company rather than having the name and title split up into multiple columns 
+e.g. [FirstName], [MiddleName], etc. Because the [Title], [MiddleName], and [Suffix] fields 
+can be NULL I have to account for that, which I do using the ISNULL function.*/
 
 SELECT 
   [Full Name] = ISNULL([title] + ' ', '') + [FirstName] + ' ' + ISNULL([MiddleName] + ' ', '') + [LastName] + ' ' + ISNULL([Suffix], '')
@@ -6,9 +10,13 @@ SELECT
 FROM [AdventureWorks2019].[Person].[Person]
 
 
---Derived Columns with mathematical calculations. Here I create four derived columns to calculate the following for Sales People in the fictional Adventure Works company: the comission year to date, the income year to date, the difference in sales from this year and last, and bonus fairnes as a percent of bonus compared to sales year to date.
+/*Derived Columns with mathematical calculations. Here I create four derived 
+columns to calculate the following for Sales People in the fictional Adventure 
+Works company: the comission year to date, the income year to date, the difference 
+in sales from this year and last, and bonus fairnes as a percent of bonus compared 
+to sales year to date.*/
 
---One particularly interesting finding is the large discrepencies we can see in the derived Bonus Fairness column (see ReadMe).
+--One particularly interesting finding is the large discrepencies we can see in the derived Bonus Fairness column.
 
 SELECT 
   [Commission Year to Date] = [SalesYTD] [CommissionPct] 
